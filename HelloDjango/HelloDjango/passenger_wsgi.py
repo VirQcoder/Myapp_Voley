28ff66f1cce4ae456ca972aa52dfc9dabbeb53d0
Abobbuss <vparipko007@gmail.com>
2023-05-02 14:34:01 +0300

    first commit

---
+++ HelloDjango/HelloDjango/passenger_wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for HelloDjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os, sys
+site_user_root_dir = '/home/v/viktok9i/viktok9i.beget.tech/public_html'
+sys.path.insert(0, site_user_root_dir + '/HelloDjango')
+sys.path.insert(1, site_user_root_dir + '/venv/lib/python3.11/site-packages/')
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HelloDjango.settings')
+
+application = get_wsgi_application()
